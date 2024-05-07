ec17fd1ef6ea6466ee9972dba7e4d266fafcb504
Ankit8125 <syncing284@gmail.com>
2024-05-07 16:51:30 +0530

    3rd proj

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Card(props){
+    return(
+        <div className="relative h-[400px] w-[300px] rounded-md">
+  <img
+    src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+    alt="AirMax Pro"
+    className="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-left">
+    <h1 className="text-lg font-semibold text-white">{props.username || "Chimp"}</h1>
+    <p className="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      View Profile →
+    </button>
+  </div>
+</div>
+
+    )
+}
+
+export default Card
